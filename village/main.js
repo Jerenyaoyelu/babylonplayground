ff1c240a6764bc882f05f0cae781a5f728e514aa
jerenluyaoye <luyaoye@xhzy.com>
2022-06-05 22:04:55 +0800

    feat：添加两个房子

--- village/main.js
+++ village/main.js
@@ -5,6 +5,71 @@ const engine = new BABYLON.Engine(canvas, true, {
   disableWebGL2Support: false,
 });
 
+const buidGround = (scene) => {
+  const ground = BABYLON.MeshBuilder.CreateGround(
+    "ground",
+    { width: 10, height: 10 },
+    scene
+  );
+
+  const groundMat = new BABYLON.StandardMaterial("groundMat", scene);
+  groundMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
+  ground.material = groundMat;
+};
+
+const buildWalls = (name, scene) => {
+  // 墙面门窗
+  const faceUV = [];
+  faceUV[0] = new BABYLON.Vector4(0.5, 0.0, 0.75, 1.0); //back face
+  faceUV[1] = new BABYLON.Vector4(0.0, 0.0, 0.25, 1.0); //front face
+  faceUV[2] = new BABYLON.Vector4(0.25, 0, 0.5, 1.0); //right side
+  faceUV[3] = new BABYLON.Vector4(0.75, 0, 1.0, 1.0); //left side
+  const box = BABYLON.MeshBuilder.CreateBox(
+    name,
+    { faceUV, wrap: true },
+    scene
+  );
+
+  box.position.y = 0.75;
+  box.scaling = new BABYLON.Vector3(2, 1.5, 3);
+  box.rotation.y = BABYLON.Tools.ToRadians(90);
+
+  // 墙面纹理
+  const boxMat = new BABYLON.StandardMaterial("boxMaterial");
+  boxMat.diffuseTexture = new BABYLON.Texture(
+    "https://assets.babylonjs.com/environments/cubehouse.png",
+    scene
+  );
+  box.material = boxMat;
+  return box;
+};
+
+const buildRoof = (scene) => {
+  const roof = BABYLON.MeshBuilder.CreateCylinder(
+    "roof",
+    { diameter: 2.6, height: 3, tessellation: 3 },
+    scene
+  );
+  roof.scaling.x = 0.8;
+  roof.rotation.z = Math.PI / 2;
+  roof.position.y = 2;
+
+  const roofMat = new BABYLON.StandardMaterial("roofMaterial");
+  roofMat.diffuseTexture = new BABYLON.Texture(
+    "https://assets.babylonjs.com/environments/roof.jpg",
+    scene
+  );
+  roof.material = roofMat;
+  return roof;
+};
+
+const createBaseHouse = async (name, scene) => {
+  const box = buildWalls(name, scene);
+  const roof = buildRoof(scene);
+
+  return { box, roof };
+};
+
 const createScene = () => {
   const scene = new BABYLON.Scene(engine);
   const camera = new BABYLON.ArcRotateCamera(
@@ -22,18 +87,29 @@ const createScene = () => {
     new BABYLON.Vector3(0, 1, 0),
     scene
   );
-  BABYLON.SceneLoader.ImportMeshAsync(
-    "",
-    "https://assets.babylonjs.com/meshes/",
-    "both_houses_scene.babylon"
-  ).then(() => {
-    const h1 = scene.getMeshByName("detached_house");
-    h1.position.x = 1;
-    h1.rotation.y = -Math.PI / 4;
-    const h2 = scene.getMeshByName("semi_house");
-    h2.rotation.y = Math.PI / 4;
-    h2.position.x = 3;
+
+  createBaseHouse("house1", scene).then(({ box, roof }) => {
+    box.position.x = -3;
+    roof.position.x = -3;
   });
+
+  createBaseHouse("house2", scene).then(({ box, roof }) => {
+    box.position.x = 3;
+    roof.position.x = 3;
+  });
+
+  buidGround();
+
+  const sound = new BABYLON.Sound(
+    "nature",
+    "./village/nature.wav",
+    scene,
+    null,
+    {
+      loop: true,
+      autoplay: true,
+    }
+  );
 
   return scene;
 };
